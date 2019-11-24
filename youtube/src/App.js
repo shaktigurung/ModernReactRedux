b49f2a4719ec4ea7780001117618cd13cd556d5d
shaktigurung <mr.shakti.gurung@gmail.com>
2019-11-24 15:41:59 +1100

    youtube video completed

--- youtube/src/App.js
+++ youtube/src/App.js
@@ -1,24 +1,47 @@
 import React from 'react';
 import youtube from './api/youtube'
 import SearchBar from './components/SearchBar';
+import VideoList from './components/VideoList';
+import VideoDetail from './components/VideoDetail';
 
 class App extends React.Component {
   
- state={videos: []};
+  state={videos: [], selectedVideo: null};
 
- onTermSubmit = async (term) => {
+  componentDidMount(){
+    this.onTermSubmit('buildings');
+  }
+
+  onTermSubmit = async (term) => {
     const response = await youtube.get('/search', {
       params: {
         q: term
       }
     })
-    this.setState({videos: response.data.items})
+    this.setState({
+      videos: response.data.items,
+      selectedVideo: response.data.items[0]
+    });
   };
+
+  onVideoSelect = (video) => {
+    this.setState({selectedVideo: video});
+  }
 
   render(){
     return (
       <div className="ui container" style={{marginTop: "10px"}}>
         <SearchBar onFormSubmit={this.onTermSubmit} />
+        <div className="ui grid">
+          <div className="ui row">
+            <div className="eleven wide column">
+              <VideoDetail video = {this.state.selectedVideo} />
+            </div>
+            <div className="five wide column">
+              <VideoList videos={this.state.videos}  onVideoSelect = {this.onVideoSelect} />
+            </div>
+          </div>
+        </div>
       </div>
     )
   }
