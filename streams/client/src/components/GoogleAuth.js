7b5c9b9d288ea74fcf00a0db02afc5d31b897728
shaktigurung <mr.shakti.gurung@gmail.com>
2019-12-01 13:37:03 +1100

    Authentication integrating in streamy app

---
+++ streams/client/src/components/GoogleAuth.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+
+class GoogleAuth extends Component {
+    state = { isSignedIn : null }
+
+    componentDidMount(){
+        window.gapi.load('client:auth2', ()=> {
+            window.gapi.client.init({
+                clientId: '524481856774-bnea7hkilrgosvv2ied8jq7d6sblofh3.apps.googleusercontent.com',
+                scope: 'email'
+            }).then(()=>{
+                this.auth = window.gapi.auth2.getAuthInstance();
+                this.setState({isSignedIn : this.auth.isSignedIn.get()})
+            });
+        });
+    }
+
+    renderAuthButton(){
+        if(this.state.isSignedIn === null) {
+            return <div> I dont know if we are signed in  </div>
+        } else if (this.state.isSignedIn) {
+            return <div> I am signed in ! </div>
+        } else {
+            return <div> I am not signed in </div>
+        }
+    }
+    render(){
+        return <div> {this.renderAuthButton()} </div>
+    }
+}
+
+export default GoogleAuth;
