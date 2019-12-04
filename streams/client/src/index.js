ce37d44aa60457449ab93cd301bd531848c4de02
shaktigurung <mr.shakti.gurung@gmail.com>
2019-12-04 18:28:15 +1100

    Redux forms and validations

--- streams/client/src/index.js
+++ streams/client/src/index.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {Provider} from 'react-redux';
-import {createStore} from 'redux';
+import {createStore, applyMiddleware, compose} from 'redux';
 
 import App from './components/App';
 import reducers from './reducers';
 
-const store = createStore(reducers);
+//Setting Redux Devtools
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(
+    reducers, 
+    composeEnhancers(applyMiddleware())
+);
+
+//const store = createStore(reducers);
 
 ReactDOM.render(
     <Provider store ={store}>
