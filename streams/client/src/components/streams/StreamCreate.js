d2e6b2504a02ffcbd58b5b0f4d10f467041ab903
shaktigurung <mr.shakti.gurung@gmail.com>
2019-12-05 14:21:21 +1100

    Stream actions applied

--- streams/client/src/components/streams/StreamCreate.js
+++ streams/client/src/components/streams/StreamCreate.js
@@ -1,5 +1,7 @@
 import React, {Component} from 'react';
 import {Field, reduxForm} from 'redux-form';
+import {createStream} from './../../actions/';
+import {connect} from 'react-redux';
 
 class StreamCreate extends Component {
 
@@ -36,8 +38,9 @@ class StreamCreate extends Component {
     //     );
     // }
 
-    onSubmit(formValues) {
-        console.log(formValues);
+    onSubmit = (formValues) => {
+        //console.log(formValues);
+        this.props.createStream(formValues);
     }
 
     render(){
@@ -62,7 +65,9 @@ const validate = (formValues) => {
     return errors;
 }
 
-export default reduxForm({
+const formWrapped = reduxForm({
     form: 'streamCreate',
     validate
 })(StreamCreate);
+
+export default  connect(null, {createStream})(formWrapped);
