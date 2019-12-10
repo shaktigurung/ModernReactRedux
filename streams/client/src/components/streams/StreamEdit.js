359de6abf2cbf26ca9f9e7c5de093781d7553afa
shaktigurung <mr.shakti.gurung@gmail.com>
2019-12-10 16:45:34 +1100

    Delete feature integrated

--- streams/client/src/components/streams/StreamEdit.js
+++ streams/client/src/components/streams/StreamEdit.js
@@ -1,11 +1,42 @@
 import React from 'react';
+import _ from 'lodash';
+import {connect} from 'react-redux';
+import {fetchStream, editStream} from './../../actions';
+import StreamForm from './StreamForm';
 
-const StreamEdit = () => {
-    return (
-        <div>
-            Stream Edit
-        </div>
-    );
+class StreamEdit extends React.Component {
+   componentDidMount(){
+    this.props.fetchStream(this.props.match.params.id);
+   }
+
+   onSubmit = (formValues) => {
+       this.props.editStream(this.props.match.params.id, formValues);
+   }
+    render(){
+        
+        if(!this.props.stream) {
+            return <div> Loading... </div>
+        }
+
+        //const {title, description} = this.props.stream;
+        return (
+            <div>
+                <h3>Edit a Stream </h3>
+                <StreamForm 
+                 initialValues={_.pick(this.props.stream,'title', 'description' )}
+                 //Alternative way to initialize value for the edit form
+                 //initialValues = {{title, description}}
+                 onSubmit={this.onSubmit} 
+                 />
+            </div>
+        );
+    }
 }
 
-export default StreamEdit;
+
+//ownProps gives access to the props of SteamEdit to mapStateToProps
+const mapStateToProps = (state, ownProps) => {
+    return {stream: state.streams[ownProps.match.params.id] }
+}
+
+export default connect(mapStateToProps, {fetchStream, editStream})(StreamEdit);
